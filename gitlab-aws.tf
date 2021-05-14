c7a8f8bbb818dc31a7145f2542c854584039a1c0
Michael Vilain <michael@vilain.com>
2021-05-13 21:21:42 -0700

    move aws network to aws-vpc file; rename of disabled tf files

--- gitlab-aws.tf
+++ gitlab-aws.tf
@@ -16,26 +16,7 @@ data "aws_region" "current" {}
 
 //================================================== GENERATE KEYS AND SAVE
 
-// ================================================== NETWORK + SUBNETS
-# data "aws_vpc" "default" {
-#   default    = true
-# }
-# data "aws_subnet_ids" "default" {
-# vpc_id    = data.aws_vpc.default.id
-# }
-# data.aws_subnet_ids.default.ids lists region's default subnet ids
-
-# data "aws_availability_zones" "available" {
-#   state    = "available"
-# }
-# data.aws_availability_zones.available.names is lists region's availability zones
-# data.aws_availability_zones.available.zone_ids is lists region's availability zone ids
-
-# Create a VPC
-# resource "aws_vpc" "example" {
-#   cidr_block  = "192.168.10.0/24"
-# }
-
+// ================================================== NETWORK + SUBNETS (in gitlab-aws-vpc.tf)
 
 //================================================== INSTANCES
 # AlmaLinux 8.3 AMI
