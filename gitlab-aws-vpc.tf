f0553581e26685f5a66e36b1959fffb0d37da5e0
Michael Vilain <michael@vilain.com>
2021-05-22 09:35:16 -0700

    add code for gitlab vpc

--- gitlab-aws-vpc.tf
+++ gitlab-aws-vpc.tf
@@ -1,23 +1,31 @@
 // aws-gitlab-vpc.tf -- define the gitlab aws vpc
-//================================================== VARIABLES (in gitlab-vars.tf)
-//================================================== PROVIDERS (in gitlab-providers.tf)
-//================================================== S3 BACKEND (in gitlab-aws-s3-backend.tf)
-// ================================================= NETWORK+SUBNETS (in gitlab-aws-vpc.tf)
-# data "aws_vpc" "default" {
-#   default    = true
-# }
-# data "aws_subnet_ids" "default" {
-# vpc_id    = data.aws_vpc.default.id
-# }
-# data.aws_subnet_ids.default.ids lists region's default subnet ids
+// ================================================= NETWORK+SUBNETS
+## data "aws_vpc" "default" {
+##   default    = true
+## }
+## data "aws_subnet_ids" "default" {
+## vpc_id    = data.aws_vpc.default.id
+## }
+### data.aws_subnet_ids.default.ids lists region's default subnet ids
 
-# data "aws_availability_zones" "available" {
-#   state    = "available"
-# }
-# data.aws_availability_zones.available.names is lists region's availability zones
-# data.aws_availability_zones.available.zone_ids is lists region's availability zone ids
+## data "aws_availability_zones" "available" {
+##   state    = "available"
+## }
+### data.aws_availability_zones.available.names is lists region's availability zones
+### data.aws_availability_zones.available.zone_ids is lists region's availability zone ids
 
-# Create a VPC
 # resource "aws_vpc" "gitlab_vpc" {
-#   cidr_block  = "192.168.10.0/24"
+#   cidr_block           = "192.168.0.0/16"
+#   enable_dns_hostnames = true
+# }
+#
+# resource "aws_internet_gateway" "gitlab_gw" {
+#   vpc_id = aws_vpc.gitlab_vpc.id
+# }
+# resource "aws_subnet" "gitlab_subnet" {
+#   vpc_id                  = aws_vpc.gitlab_vpc.id
+#   cidr_block              = "192.168.10.0/24"
+#   map_public_ip_on_launch = true
+#
+#   depends_on = [aws_internet_gateway.gitlab_gw]
 # }
