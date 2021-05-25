a3d7ad55717a0f55cd8eb8c008388a6756e82718
Michael Vilain <michael@vilain.com>
2021-05-24 23:20:08 -0700

    move security groups to aws-vpc-sg.tf

--- aws-vpc.tf
+++ aws-vpc.tf
@@ -1,5 +1,5 @@
 // aws-vpc.tf -- define the gitlab aws vpc
-// ================================================= NETWORK+SUBNETS
+// ================================================= NETWORK+SUBNETS+ACLs
 data "aws_vpc" "default" {
   default    = true
 }
@@ -46,7 +46,6 @@ resource "aws_internet_gateway" "gitlab_gw" {
   }
 }
 
-
 resource "aws_route_table" "gitlab_rtb" {
   vpc_id = aws_vpc.gitlab_vpc.id
   route {
@@ -64,9 +63,10 @@ resource "aws_route_table_association" "github_subnet_rtb" {
   route_table_id = aws_route_table.gitlab_rtb.id
 }
 
-# apply network ACLs to VPC to restrict access to entire VPC 
-# rather Security Groups which are per instance
-# sadly network_acl_rules don't take descriptions
+## apply network ACLs to VPC to restrict access to entire VPC
+## rather Security Groups which are per instance
+## sadly network_acl_rules don't take descriptions
+##
 resource "aws_network_acl" "gitlab_acl" {
   vpc_id      = aws_vpc.gitlab_vpc.id
   subnet_ids  = [ aws_subnet.gitlab_subnet.id ]
@@ -75,7 +75,9 @@ resource "aws_network_acl" "gitlab_acl" {
     Terraform = "True"
   }
 }
-#
+
+## default network_acl settings
+##
 resource "aws_network_acl_rule" "gitlab_acl_egress" {
   network_acl_id = aws_network_acl.gitlab_acl.id
   rule_number    = 200
@@ -95,142 +97,4 @@ resource "aws_network_acl_rule" "gitlab_acl_http" {
   cidr_block     = "0.0.0.0/0"
   from_port      = 0
   to_port        = 0
-}
-#resource "aws_network_acl_rule" "gitlab_acl_http" {
-#  network_acl_id = aws_network_acl.gitlab_acl.id
-#  rule_number    = 100
-#  egress         = false
-#  protocol       = "tcp"
-#  rule_action    = "allow"
-#  cidr_block     = "0.0.0.0/0"
-#  from_port      = 80
-#  to_port        = 80
-#}
-#resource "aws_network_acl_rule" "gitlab_acl_https" {
-#  network_acl_id = aws_network_acl.gitlab_acl.id
-#  rule_number    = 110
-#  egress         = false
-#  protocol       = "tcp"
-#  rule_action    = "allow"
-#  cidr_block     = "0.0.0.0/0"
-#  from_port      = 443
-#  to_port        = 443
-#}
-#resource "aws_network_acl_rule" "gitlab_acl_ssh_home" {
-#  network_acl_id = aws_network_acl.gitlab_acl.id
-#  rule_number    = 120
-#  egress         = false
-#  protocol       = "tcp"
-#  rule_action    = "allow"
-#  cidr_block     = "75.25.136.0/24"
-#  from_port      = 22
-#  to_port        = 22
-#}
-#resource "aws_network_acl_rule" "gitlab_acl_ssh_nord" {
-#  network_acl_id = aws_network_acl.gitlab_acl.id
-#  rule_number    = 130
-#  egress         = false
-#  protocol       = "tcp"
-#  rule_action    = "allow"
-#  cidr_block     = "192.145.118.0/24"
-#  from_port      = 22
-#  to_port        = 22
-#}
-#resource "aws_network_acl_rule" "gitlab_acl_ssh_leaseweb80" {
-#  network_acl_id = aws_network_acl.gitlab_acl.id
-#  rule_number    = 180
-#  egress         = false
-#  protocol       = "tcp"
-#  rule_action    = "allow"
-#  cidr_block     = "23.80.0.0/15"
-#  from_port      = 22
-#  to_port        = 22
-#}
-#resource "aws_network_acl_rule" "gitlab_acl_ssh_leaseweb82" {
-#  network_acl_id = aws_network_acl.gitlab_acl.id
-#  rule_number    = 182
-#  egress         = false
-#  protocol       = "tcp"
-#  rule_action    = "allow"
-#  cidr_block     = "23.82.0.0/16"
-#  from_port      = 22
-#  to_port        = 22
-#}
-#resource "aws_network_acl_rule" "gitlab_acl_ssh_leaseweb83" {
-#  network_acl_id = aws_network_acl.gitlab_acl.id
-#  rule_number    = 183
-#  egress         = false
-#  protocol       = "tcp"
-#  rule_action    = "allow"
-#  cidr_block     = "23.83.0.0/18"
-#  from_port      = 22
-#  to_port        = 22
-#}
-
-
-resource "aws_security_group" "gitlab_sg" {
-  name        = "gitlab_sg"
-  description = "Allow gitlab inbound traffic"
-  vpc_id      = aws_vpc.gitlab_vpc.id
-
-  tags = {
-    Name      = "gitlab-sg",
-    Terraform = "True"
-  }
-}
-#
-# gitlab's Let's Encrypt won't authenticate unless this is open
-#
-resource "aws_security_group_rule" "gitlab_sgr_egress" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = [ "0.0.0.0/0" ]
-  security_group_id = aws_security_group.gitlab_sg.id
-}
-#resource "aws_security_group_rule" "gitlab_sgr_all" {
-#  type              = "ingress"
-#  from_port         = 0
-#  to_port           = 0
-#  protocol          = "-1"
-#  cidr_blocks       = [ "0.0.0.0/0" ]
-#  security_group_id = aws_security_group.gitlab_sg.id
-#}
-
-resource "aws_security_group_rule" "gitlab_sgr_ssh_home" {
-  type              = "ingress"
-  description       = "gitlab ssh home"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  cidr_blocks       = [ "75.25.136.0/24" ]
-  security_group_id = aws_security_group.gitlab_sg.id
-}
-resource "aws_security_group_rule" "gitlab_sgr_ssh_leaseweb80" {
-  type              = "ingress"
-  description       = "gitlab ssh leaseweb80"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  cidr_blocks       = [ "23.80.0.0/15" ]
-  security_group_id = aws_security_group.gitlab_sg.id
-}
-resource "aws_security_group_rule" "gitlab_sgr_http" {
-  type              = "ingress"
-  description       = "gitlab http"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  cidr_blocks       = [ "0.0.0.0/0" ]
-  security_group_id = aws_security_group.gitlab_sg.id
-}
-resource "aws_security_group_rule" "gitlab_sgr_https" {
-  type              = "ingress"
-  description       = "gitlab https"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
-  cidr_blocks       = [ "0.0.0.0/0" ]
-  security_group_id = aws_security_group.gitlab_sg.id
 }
